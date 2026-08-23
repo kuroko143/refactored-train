97f17e0cc06ca613362210e281abccedfe40e6eb
kuroko143 <jchan.utoronto@gmail.com>
2026-08-23 12:12:48 -0400

    optimize build time

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,16 +6,6 @@ cp -avf "/ctx/system_files"/. /
 
 dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/docker-ce.repo
-dnf -y install --enablerepo=docker-ce-stable \
-    containerd.io \
-    docker-buildx-plugin \
-    docker-ce \
-    docker-ce-cli \
-    docker-compose-plugin \
-    docker-model-plugin
-
-systemctl enable docker.service docker.socket
-systemctl enable podman.socket
 
 tee /etc/yum.repos.d/vscode.repo <<'EOF'
 [code]
@@ -26,28 +16,40 @@ gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/vscode.repo
-dnf -y install --enablerepo=code code
 
-dnf5 -y install \
+cat <<EOF > /etc/yum.repos.d/adoptium.repo
+[Adoptium]
+name=Adoptium
+baseurl=https://packages.adoptium.net/artifactory/rpm/fedora/\$releasever/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
+enabled_metadata=1
+EOF
+
+dnf5 -y copr enable atim/starship
+dnf5 -y copr enable avengemedia/danklinux
+dnf5 -y copr enable ulysg/xwayland-satellite
+
+dnf5 -y install --enablerepo=docker-ce-stable,code \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin \
+    docker-model-plugin \
+    code \
     fastfetch \
     p7zip \
     p7zip-plugins \
     stow \
     unzip \
-    zip
-
-dnf5 -y copr enable atim/starship
-dnf5 -y install \
+    zip \
     atuin \
     eza \
     kitty \
     neovim \
-    starship 
-dnf5 -y copr disable atim/starship
-
-dnf5 -y copr enable avengemedia/danklinux
-dnf5 -y copr enable ulysg/xwayland-satellite
-dnf5 -y install \
+    starship \
     blueman \
     dms \
     dms-greeter \
@@ -63,21 +65,7 @@ dnf5 -y install \
     xdg-desktop-portal \
     xdg-desktop-portal-gnome \
     xdg-desktop-portal-gtk \
-    xwayland-satellite
-dnf5 -y copr disable avengemedia/danklinux
-dnf5 -y copr disable ulysg/xwayland-satellite
-
-cat <<EOF > /etc/yum.repos.d/adoptium.repo
-[Adoptium]
-name=Adoptium
-baseurl=https://packages.adoptium.net/artifactory/rpm/fedora/\$releasever/\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
-enabled_metadata=1
-EOF
-
-dnf5 -y install \
+    xwayland-satellite \
     ark \
     dolphin \
     file-roller \
@@ -89,6 +77,12 @@ dnf5 -y install \
     playerctl \
     temurin-11-jdk \
     temurin-8-jdk
+
+dnf5 -y copr disable atim/starship
+dnf5 -y copr disable avengemedia/danklinux
+dnf5 -y copr disable ulysg/xwayland-satellite
+
+systemctl enable docker.service docker.socket podman.socket
 
 systemctl disable gdm.service
 systemctl mask gdm.service
